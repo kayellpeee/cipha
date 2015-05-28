343b688172ad915d99079e06f73ea739d73e292f
Keenan Lidral-Porter <klp2012@gmail.com>
2015-05-28 00:05:35 -0700

    [feat] compiles basic feistel_encrypt function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(collections)]
 /// encryption is similar to hashing without destructive operation
 /// it also requires a key - or something to encrypt against
 /// For a Feistel cipher we'll have to generate subkeys from the master key
@@ -7,16 +8,21 @@
 /// This will take a simpler approach so decryption subkey generation is easier
 fn feistel_encrypt(message: &str, key: u8, rounds: u8) -> &str {
     let mut left: Vec<_> = message.bytes().collect();
-    let mut right: Vec<_> = left.split_off(left.len() / 2);
+    let message_length: usize = left.len();
+    let mut right: Vec<_> = left.split_off(message_length / 2);
     let mut subkey: u8;
     let mut updated_left: Vec<_>;
     let mut updated_right: Vec<_>;
     for x in 0..rounds {
         subkey = key.rotate_right(x as u32);
         // L[i] = R[i - 1]
-        updated_left = right;
+        updated_left = right.clone();
         // R[i] = L[i - 1] ⊕ f(r[i - 1], k[i])
-        updated_right = left ^ encrypt_helper(right, subkey);
+        updated_right = Vec::new();
+        right = encrypt_helper(right, subkey);
+        for i in 0..left.len() {
+            updated_right.push(left[i] ^ right[i]);
+        }
         right = updated_right;
         left = updated_left;
     }
@@ -25,12 +31,14 @@ fn feistel_encrypt(message: &str, key: u8, rounds: u8) -> &str {
     message
 }
 fn encrypt_helper(right: Vec<u8>, subkey: u8) -> Vec<u8> {
+    let mut added_right = Vec::new();
     println!("before running helper - {:?}", right);
-    for byte in right {
+    for mut byte in right.clone() {
         byte += subkey;
+        added_right.push(byte);
     }
-    println!("ran helper with subkey {:?} - {:?}", subkey, right);
-    right
+    println!("ran helper with subkey {:?} - {:?}", subkey, added_right);
+    added_right
 }
 #[test]
 fn it_works() {
