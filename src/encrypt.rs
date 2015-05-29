a2da67d600b055efe6cdfe60982eef6eeb838232
Keenan Lidral-Porter <klp2012@gmail.com>
2015-05-28 22:18:54 -0700

    [feat] converts encrypted str bytes to String

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -5,7 +5,7 @@
 /// One approach would be to hash a key to generate the subkey, which is hashed
 /// to generate the next subkey...so on so forth
 /// This will take a simpler approach so decryption subkey generation is easier
-pub fn feistel_encrypt(message: &str, key: u8, rounds: u8) -> &str {
+pub fn feistel_encrypt(message: &str, key: u8, rounds: u8) -> String {
     let mut left: Vec<u8> = message.bytes().collect();
     let message_length: usize = left.len();
     let mut right: Vec<u8> = left.split_off(message_length / 2);
@@ -29,7 +29,13 @@ pub fn feistel_encrypt(message: &str, key: u8, rounds: u8) -> &str {
     }
     println!("Finished all {:?} rounds\nleft {:?}\nright {:?}",
              rounds, left, right);
-    message
+    let encrypted_left = String::from_utf8(left).unwrap();
+    let encrypted_right = String::from_utf8(right).unwrap();
+    let mut encrypted_message: String = String::new();
+    encrypted_message.push_str(&encrypted_left);
+    encrypted_message.push_str(&encrypted_right);
+    println!("fully encrypted - {:?}", &encrypted_message);
+    encrypted_message
 }
 
 fn encrypt_helper(right: Vec<u8>, subkey: u8) -> Vec<u8> {
