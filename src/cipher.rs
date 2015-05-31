41e4ebf5ab9bbd3a941ebe2797fd55f5b17ab2ab
Keenan Lidral-Porter <klp2012@gmail.com>
2015-05-30 21:39:35 -0700

    [feat] adds basic decryption function

--- src/cipher.rs
+++ src/cipher.rs
@@ -46,3 +46,46 @@ fn encrypt_helper(right: Vec<u8>, subkey: u8) -> Vec<u8> {
     added_right
 }
 
+pub fn feistel_decrypt(ciphertext: &str, key: u8, rounds: u8) -> String {
+    let mut left: Vec<u8> = ciphertext.bytes().collect();
+    let ciphertext_length: usize = left.len();
+    let mut right: Vec<u8> = left.split_off(ciphertext_length / 2);
+    let mut subkey: u8;
+    let mut updated_left: Vec<u8>;
+    let mut updated_right: Vec<u8>;
+    println!("About to decrypt!\nleft {:?}\nright {:?}", left, right);
+    for x in 0..rounds {
+        // only difference in encryption & decryption is order of subkeys
+        // and reversible round function
+        subkey = key.rotate_right((rounds - x) as u32);
+        // l[i] = R[i - 1]
+        updated_left = right.clone();
+        // R[i] = L[i - 1] ⊕ f(r[i - 1], k[i])
+        updated_right = Vec::new();
+        right = decrypt_helper(right, subkey);
+        for i in 0..left.len() {
+            updated_right.push(left[i] ^ right[i]);
+        }
+        right = updated_right;
+        left = updated_left;
+    }
+    println!("finished all {:?} rounds of decryption!\nleft {:?}\nright {:?}",
+             rounds, left, right);
+    let decrypted_left = String::from_utf8(left).unwrap();
+    let decrypted_right = String::from_utf8(right).unwrap();
+    let mut plaintext: String = String::new();
+    plaintext.push_str(&decrypted_left);
+    plaintext.push_str(&decrypted_right);
+    println!("fully decrypted!\n{:?}", &plaintext);
+    plaintext
+}
+
+fn decrypt_helper(right: Vec<u8>, subkey: u8) -> Vec<u8> {
+    let mut added_right = Vec::new();
+    for byte in right.clone() {
+        byte.wrapping_add(subkey);
+        added_right.push(byte);
+    }
+    added_right
+}
+
