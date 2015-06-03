9c5e643da71d16c2b97c010d5ce0d94aafcb77ce
Keenan Lidral-Porter <klp2012@gmail.com>
2015-06-02 20:43:10 -0700

    [feat] encrypts to & decrypts from Vec<u32> (++encryption range)

--- src/cipher.rs
+++ src/cipher.rs
@@ -5,13 +5,15 @@
 /// One approach would be to hash a key to generate the subkey, which is hashed
 /// to generate the next subkey...so on so forth
 /// This will take a simpler approach so decryption subkey generation is easier
-pub fn feistel_encrypt(plaintext: &str, key: u8, rounds: u8) -> Vec<u8> {
-    let mut left: Vec<u8> = plaintext.bytes().collect();
-    let plaintext_length: usize = left.len();
-    let mut right: Vec<u8> = left.split_off(plaintext_length / 2);
-    let mut subkey: u8;
-    let mut updated_left: Vec<u8>;
-    let mut updated_right: Vec<u8>;
+pub fn feistel_encrypt(plaintext: &str, key: u32, rounds: u8) -> Vec<u32> {
+    let mut _left: Vec<u8> = plaintext.bytes().collect();
+    let plaintext_length: usize = _left.len();
+    let mut _right: Vec<u8> = _left.split_off(plaintext_length / 2);
+    let mut left: Vec<u32> = _left.into_iter().map(|x| x as u32).collect();
+    let mut right: Vec<u32> = _right.into_iter().map(|x| x as u32).collect();
+    let mut subkey: u32;
+    let mut updated_left: Vec<u32>;
+    let mut updated_right: Vec<u32>;
     println!("About to encrypt!\nleft {:?}\nright {:?}", left, right);
     for x in 0..rounds {
         subkey = key.clone().rotate_right(x as u32);
@@ -40,28 +42,28 @@ pub fn feistel_encrypt(plaintext: &str, key: u8, rounds: u8) -> Vec<u8> {
         println!("On round {:?}/{:?} of encryption\n\tleft {:?}\n\tright {:?}
                 \tsubkey {:?}", x + 1, rounds, left, right, subkey);
     }
-    let mut _ciphertext: Vec<u8> = left.clone();
+    let mut _ciphertext: Vec<u32> = left.clone();
     _ciphertext.append(&mut right);
     let ciphertext = _ciphertext;
     println!("fully encrypted - {:?}", &ciphertext);
     ciphertext
 }
 
-fn encrypt_helper(right: Vec<u8>, subkey: u8) -> Vec<u8> {
+fn encrypt_helper(right: Vec<u32>, subkey: u32) -> Vec<u32> {
     let mut added_right = Vec::new();
     for byte in right.clone() {
-        byte.wrapping_add(subkey);
+        byte.wrapping_add(subkey as u32);
         added_right.push(byte);
     }
     added_right
 }
 
-pub fn feistel_decrypt(ciphertext: Vec<u8>, key: u8, rounds: u8) -> String {
-    let mut right: Vec<u8> = ciphertext.clone();
-    let mut left: Vec<u8> = right.split_off(ciphertext.len() / 2);
-    let mut subkey: u8;
-    let mut updated_left: Vec<u8>;
-    let mut updated_right: Vec<u8>;
+pub fn feistel_decrypt(ciphertext: Vec<u32>, key: u32, rounds: u8) -> String {
+    let mut right: Vec<u32> = ciphertext.clone();
+    let mut left: Vec<u32> = right.split_off(ciphertext.len() / 2);
+    let mut subkey: u32;
+    let mut updated_left: Vec<u32>;
+    let mut updated_right: Vec<u32>;
     // because encryption went from [0, rounds) decryption should
     // generate subkeys for (rounds, 0]
     for x in 1..rounds + 1 {
@@ -93,8 +95,10 @@ pub fn feistel_decrypt(ciphertext: Vec<u8>, key: u8, rounds: u8) -> String {
         println!("On round {:?}/{:?} of decryption\n\tleft {:?}\n\tright {:?}
                  \tsubkey {:?}", x, rounds, left, right, subkey);
     }
-    let decrypted_left = String::from_utf8(right).unwrap();
-    let decrypted_right = String::from_utf8(left).unwrap();
+    let right_u8 = right.into_iter().map(|x| x as u8).collect();
+    let left_u8 = left.into_iter().map(|x| x as u8).collect();
+    let decrypted_left = String::from_utf8(right_u8).unwrap();
+    let decrypted_right = String::from_utf8(left_u8).unwrap();
     let mut plaintext: String = String::new();
     plaintext.push_str(&decrypted_left);
     plaintext.push_str(&decrypted_right);
@@ -102,7 +106,7 @@ pub fn feistel_decrypt(ciphertext: Vec<u8>, key: u8, rounds: u8) -> String {
     plaintext
 }
 
-fn decrypt_helper(right: Vec<u8>, subkey: u8) -> Vec<u8> {
+fn decrypt_helper(right: Vec<u32>, subkey: u32) -> Vec<u32> {
     let mut subtracted_right = Vec::new();
     for byte in right.clone() {
         byte.wrapping_sub(subkey);
