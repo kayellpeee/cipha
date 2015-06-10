e114c1b63ef16ec2e3655224924b5dbdd74d8b53
Keenan Lidral-Porter <klp2012@gmail.com>
2015-06-09 23:47:40 -0700

    [test] moves simple tests into it_works()

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,9 @@ fn it_works() {
     let ciphertext = feistel_encrypt("keenan", 19, 4);
     let plaintext = feistel_decrypt(ciphertext, 19, 4);
     assert_eq!("keenan", plaintext);
+    let odd_ciphertext = feistel_encrypt("ricky", 110, 2);
+    let odd_plaintext = feistel_decrypt(odd_ciphertext, 110, 2);
+    assert_eq!("ricky", odd_plaintext);
 }
 /// One "metric" of an encryption protocol's strength is how little relation
 /// there is between the inputs and outputs. AKA there must be either
@@ -22,8 +25,4 @@ fn it_works() {
 /// output and see if similar inputs map to similar outputs (not random) as long
 /// as all inupts don't map to similar outputs (consistent).
 #[test]
-fn odd_length_message() {
-    let ciphertext = feistel_encrypt("ricky", 110, 2);
-    let plaintext = feistel_decrypt(ciphertext, 110, 2);
-    assert_eq!("ricky", plaintext);
 }
