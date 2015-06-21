9b125a3b460b7802199b113b6da3a6cf7c6d46f0
Keenan Lidral-Porter <klp2012@gmail.com>
2015-06-20 18:49:40 -0700

    [test] tests variance in ciphertext bytes

--- src/lib.rs
+++ src/lib.rs
@@ -21,9 +21,9 @@ mod feistel_tests {
         let odd_plaintext = feistel_decrypt(odd_ciphertext, 110, 2);
         assert_eq!("ricky", odd_plaintext);
 
-        let odd_rounds_ciphertext = feistel_encrypt("julian", 92, 5);
-        let odd_rounds_plaintext = feistel_decrypt(odd_rounds_ciphertext, 92, 5);
-        assert_eq!("julian", odd_rounds_plaintext);
+        let odd_round_ciphertext = feistel_encrypt("julian", 92, 5);
+        let odd_round_plaintext = feistel_decrypt(odd_round_ciphertext, 92, 5);
+        assert_eq!("julian", odd_round_plaintext);
     }
 
     #[test]
@@ -70,73 +70,33 @@ mod feistel_tests {
     ///  or
     ///  "a" -> 1.013, "b" -> 1.003, "c" -> 1.201, "de" -> 1.031
     /// Both are really hard to draw patterns from & thus reverse engineer the
-    /// encryption algorithm. To test this we'll find the difference b/w input and
-    /// output and see if similar inputs map to similar outputs (not random) as long
-    /// as all inupts don't map to similar outputs (consistent).
+    /// encryption algorithm. To test this we'll find the difference b/w input
+    /// and output and see if similar inputs map to similar outputs (not random)
+    /// as long as all inupts don't map to similar outputs (consistent).
+    ///   note: This is not necessarily a test of the code or this
+    ///   implementation of a feistel cipher, but a test of feistel ciphers
+    ///   themselve. Take it with a grain of salt
     #[test]
-    fn delta() {
-        let plain_alphabet = "abcdefghijklmnopqrstuvwxyz";
-        let cipher_alphabet = feistel_encrypt(plain_alphabet, 42, 5);
-        let mut delta: u32;
-        let mut average_delta: u32 = 0;
-        let mut max_delta: u32 = 0;
-        for tuple in plain_alphabet.bytes().enumerate() {
-            if cipher_alphabet[tuple.0] > tuple.1 as u32 {
-                delta = cipher_alphabet[tuple.0] - tuple.1 as u32;
-            } else {
-                delta =  tuple.1 as u32 - cipher_alphabet[tuple.0];
-            }
-            average_delta += delta / cipher_alphabet.len() as u32;
-            if delta > max_delta {
-               max_delta = delta;
-            }
+    fn variance() {
+        // all a's - should either have flat variance (nothing outside a certain
+        // range) - or "chaotic" variance (no large amount in same relatively
+        // small range
+        let plain_ayes = "abcdefghijklmnopqrstuvwxyz";
+        let cipher_ayes = feistel_encrypt(plain_ayes, 186220, 217);
+        let mean: f32= (cipher_ayes.iter().fold(0, |sum, x| sum + x)
+            / cipher_ayes.len() as u32) as f32;
+        let mut variance: f32 = 0_f32;
+        for x in cipher_ayes.iter() {
+            variance += (*x as f32 - mean).powi(2);
         }
-        let plain_alphabet_2 = "abcdefghijklmnopqrstuvwxyz";
-        let cipher_alphabet_2 = feistel_encrypt(plain_alphabet, 20150406, 87);
-        let mut delta_2: u32;
-        let mut average_delta_2: u32 = 0;
-        let mut max_delta_2: u32 = 0;
-        for tuple in plain_alphabet_2.bytes().enumerate() {
-            if cipher_alphabet_2[tuple.0] > tuple.1 as u32 {
-                delta_2 = cipher_alphabet_2[tuple.0] - tuple.1 as u32;
-            } else {
-                delta_2 =  tuple.1 as u32 - cipher_alphabet_2[tuple.0];
-            }
-            average_delta_2 += delta_2 / cipher_alphabet_2.len() as u32;
-            if delta_2 > max_delta_2 {
-               max_delta_2 = delta_2;
-            }
-        }
-        println!("key 42 5 rounds max {:?} avg {:?}", max_delta, average_delta);
-        println!("key 20150406 87 rounds rounds max {:?} avg {:?}",
-                 max_delta_2, average_delta_2);
-        assert!(max_delta != max_delta_2);
-        assert!(average_delta != average_delta_2);
-
-        // These aren't really tests, just printing results to get a sense of what's
-        // going on. Well set up defined tests later, which will probably be similar
-        // to benchmark tests——used for insight not testing code functionality
-        let plain_message = "Perhaps it was because I was a younger man and more
-        impressionable.";
-        let cipher_message = feistel_encrypt(plain_message, 1381964, 32);
-        average_delta = 0;
-        max_delta = 0;
-        for tuple in plain_message.bytes().enumerate() {
-            if cipher_message[tuple.0] > tuple.1 as u32 {
-                delta = cipher_message[tuple.0] - tuple.1 as u32;
-            } else {
-                delta =  tuple.1 as u32 - cipher_message[tuple.0];
-            }
-           average_delta += delta / cipher_message.len() as u32;
-           if delta > max_delta {
-               max_delta = delta;
-           }
-        }
-        println!("average delta\t {:?}", average_delta);
-        println!("max delta\t\t {:?}", max_delta);
-        // Main test wouldn't be in the max, min or average difference between plain
-        // vs cipher text, but in the variance b/w the two. Want either no variance
-        // or unpredictable....will be hard to test for unpredictable variance
+        variance = variance / cipher_ayes.len() as f32;
+        let standard_deviation = variance.sqrt();
+        // let's say encrypted bytes should have a standard deviation at least
+        // 40% of the mean OR 5% of it (very large vs. very small variance)
+        let teir_1: f32 = 0.40;
+        let teir_2: f32 = 0.05;
+        assert!(standard_deviation / mean > teir_1 ||
+               standard_deviation / mean < teir_2);
     }
     #[bench]
     fn sentence_encryption(b: &mut Bencher) {
